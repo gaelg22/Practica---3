c1849b6715ca627e20bef09f8c0e442e9ec521b9
gaelg22 <gaelg2927@gmail.com>
2026-01-05 03:13:35 -0600

    Proyecto

--- src/main/java/com/upiiz/practicaVIII/PracticaVIII/security/SecurityConfig.java
+++ src/main/java/com/upiiz/practicaVIII/PracticaVIII/security/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
                 // ENDPOINTS PÚBLICOS (Login y Register)
                 // -----------------------------------------
                 .requestMatchers("/api/auth/**").permitAll()
+                .requestMatchers("/").permitAll()
 
                 // -----------------------------------------
                 // GET públicos
